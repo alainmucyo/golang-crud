b30e45aede429559b9af16d9756f2bf294b17826
root <alainmucyo3@gmail.com>
2020-09-09 09:33:16 +0200

    Use .env variables

--- data/database/db.go
+++ data/database/db.go
@@ -2,12 +2,22 @@ package database
 
 import (
 	"database/sql"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 var con *sql.DB
 func Connect() *sql.DB {
-	db,err := sql.Open("mysql","root:password@/go_learn")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Can't load .env")
+	}
+	DB_PASSWORD:= os.Getenv("DB_PASSWORD")
+	DB_USERNAME := os.Getenv("DB_USERNAME")
+	DB_DATABASE := os.Getenv("DB_DATABASE")
+	DB_TYPE := os.Getenv("DB_TYPE")
+	db,err := sql.Open(DB_TYPE,DB_USERNAME+":"+DB_PASSWORD+"@/"+DB_DATABASE)
 	if err !=nil {
 		os.Exit(2)
 	}
--- main.go
+++ main.go
@@ -4,14 +4,22 @@ import (
 	"github.com/alainmucyo/crud/data/database"
 	"github.com/alainmucyo/crud/routes"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	//fmt.Println("Hello")
 	db := database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Can't load .env")
+	}
+	port:= os.Getenv("PORT")
+	url := os.Getenv("URL")
 	defer db.Close()
-	println("Server started....")
-	log.Fatal(http.ListenAndServe(":3000", routes.Register()))
+	println("Server started at "+url+":"+port)
+	log.Fatal(http.ListenAndServe(":"+port, routes.Register()))
 }
